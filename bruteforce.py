2241b9de4ef9ccb36fd6e781c85aa6f06074cb55
Jonathan Guyot Bourgeois <j.guyot-bourgeois@libr.im>
2022-02-03 15:40:12 +0100

    début d'implémentation du multithreading pour le bruteforce

--- bruteforce.py
+++ bruteforce.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import sys
+import threading
+from math import ceil
 import pandas as pd
 
 MAX_EXPENSE = 500
@@ -18,27 +20,49 @@ def remove_incorrect_data(stocks):
     return new_stocks
 
 
+class ValidCombinations(threading.Thread):
+    def __init__(self, range_max_combinations):
+        threading.Thread.__init__(self)
+        self.max_combinations = range_max_combinations
+
+    @classmethod
+    def data(cls, stocks, number_of_action):
+        cls.stocks = stocks
+        cls.number_of_action = number_of_action
+
+    def run(self):
+        self.valid_combinations = []
+        for combination in self.max_combinations:
+            expense, valids = 0, []
+            matrix = bin(combination)[2:]
+            matrix = '0' * (self.number_of_action - len(matrix)) + matrix
+            for i in range(self.number_of_action):
+                if matrix[i] == '1':
+                    expense += self.stocks[i][PRICE]
+                    valids.append(self.stocks[i])
+            if expense <= MAX_EXPENSE:
+                self.valid_combinations.append(valids)
+
+
 def bruteforce(stocks):
     # Calculate the profit in euro
     for stock in stocks:
         profit_euro = stock[PRICE] * (stock[PROFIT_PERCENT] / 100)
-        stock.append(round(profit_euro, 2))
+        stock.append(profit_euro)
     # Determine the maximum number of combinations
     number_of_action = len(stocks)
     max_combinations = 2 ** number_of_action
+    median_combinations = ceil(max_combinations / 2)
     print(f"Maximum combinations: {max_combinations}\n")
     # Retrieve only the list of valid combinations (<=MAX_EXPENSE)
-    valid_combinations = []
-    for combination in range(max_combinations):
-        expense, valids = 0, []
-        matrix = bin(combination)[2:]
-        matrix = '0' * (number_of_action - len(matrix)) + matrix
-        for i in range(number_of_action):
-            if matrix[i] == '1':
-                expense += stocks[i][PRICE]
-                valids.append(stocks[i])
-        if expense <= MAX_EXPENSE:
-            valid_combinations.append(valids)
+    ValidCombinations.data(stocks, number_of_action)
+    th1 = ValidCombinations(range(0, median_combinations))
+    th2 = ValidCombinations(range(median_combinations, max_combinations))
+    th1.run()
+    th2.run()
+    valid_combinations = th1.valid_combinations + th2.valid_combinations
+    # th1.join()
+    # th2.join()
     # Calculates the profit of each combination and keeps the best
     max_profit, amount_of_expense, i = 0, 0, 0
     print(f"Possible combinations: {len(valid_combinations)}\n")
