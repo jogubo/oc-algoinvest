ce800d53b8cc8f829cb7e335b093afc0f6965648
Jonathan Guyot Bourgeois <j.guyot-bourgeois@libr.im>
2022-02-04 02:08:18 +0100

    implementation du multithread terminé, gros gain sur les performances

--- bruteforce.py
+++ bruteforce.py
@@ -1,39 +1,67 @@
 #!/usr/bin/env python3
 
 import sys
-import threading
-from math import ceil
-import pandas as pd
 import time
+import pandas as pd
+from math import ceil
+from threading import Thread
 
 
-class Bruteforce(threading.Thread):
+class Bruteforce(Thread):
 
-    def __init__(self, stocks, range_combinations, thread_name):
-        threading.Thread.__init__(self)
+    def __init__(self, stocks, range_combinations):
+        Thread.__init__(self)
         self.range_combinations = range_combinations
         self.stock = stocks
-        self.thread_name = thread_name
+        self.stock_quantity = STOCKS_QUANTITY
+        self.price = PRICE
+        self.max_expense = MAX_EXPENSE
+        self.profit_euro = PROFIT_EURO
+        self.start()
 
     def run(self):
         self.max_profit = 0
         for combination in self.range_combinations:
-            # time.sleep(3)
-            # print(self.thread_name)
-            matrix = to_matrix(combination)
+            matrix = self.to_matrix(combination)
             expense, profit = 0, 0
-            for i in range(STOCKS_QUANTITY):
+            for i in range(self.stock_quantity):
+                # Convert the combination number into a matrix
                 if matrix[i] == '1':
-                    expense += stocks[i][PRICE]
-                    if expense <= MAX_EXPENSE:
-                        profit += stocks[i][PROFIT_EURO]
+                    # Calculates the profit
+                    if expense <= self.max_expense:
+                        profit += stocks[i][self.profit_euro]
+            # Get the best
             if profit > self.max_profit:
                 self.max_profit = profit
                 self.total_cost = expense
                 self.best_invest = combination
 
+    def to_matrix(self, number):
+        """
+        Convert the combination number into a matrix.
+
+            Parameters:
+                number (int): The number of combination
+
+            Returns:
+                matrix (str): The matrix of combination
+        """
+        matrix = bin(number)[2:]
+        matrix = '0' * (self.stock_quantity - len(matrix)) + matrix
+        return matrix
+
 
 def remove_incorrect_data(stocks):
+    """
+    Remove unnecessary or incorrect lines.
+
+            Parameters:
+                stocks (list): List of stocks
+
+            Returns:
+                new_stocks (list)
+                total_incorrects (int)
+    """
     new_stocks, total_incorrects = [], 0
     for stock in stocks:
         if stock[PRICE] > 0 and stock[PROFIT_PERCENT] > 0:
@@ -43,15 +71,19 @@ def remove_incorrect_data(stocks):
     return new_stocks, total_incorrects
 
 
-def to_matrix(number):
+def to_combination(number):
+    """
+    Get a list of stocks from combination number.
+
+        Parameters:
+            number (int): The number of combination
+
+        Returns:
+            combination (list): List of stocks
+    """
+    combination = []
     matrix = bin(number)[2:]
     matrix = '0' * (STOCKS_QUANTITY - len(matrix)) + matrix
-    return matrix
-
-
-def to_combination(stocks, number):
-    combination = []
-    matrix = to_matrix(number)
     for i in range(STOCKS_QUANTITY):
         if matrix[i] == '1':
             combination.append(stocks[i])
@@ -64,6 +96,7 @@ def to_combination(stocks, number):
 if __name__ == "__main__":
     MAX_EXPENSE = 500
     NAME, PRICE, PROFIT_PERCENT, PROFIT_EURO = 0, 1, 2, 3
+    start_time = time.time()
     if len(sys.argv) < 2:
         print("You must specify a dataframe file  as argument.")
     elif len(sys.argv) != 2:
@@ -89,33 +122,27 @@ if __name__ == "__main__":
         MAX_CONBINATION = 2 ** STOCKS_QUANTITY
         MEDIAN_COMBINATION = ceil(MAX_CONBINATION / 2)
         print(f"Maximum combinations: {MAX_CONBINATION}\n")
-        # Retrieve only the list of valid combinations (<=MAX_EXPENSE)
+        # Start bruteforce on multiple threads
         thread_1 = Bruteforce(
             stocks,
             range(0, MEDIAN_COMBINATION),
-            "Thread_1"
         )
-        thread_1.start()
         thread_2 = Bruteforce(
             stocks,
-            range(MAX_CONBINATION, MAX_CONBINATION),
-            "Thread_2"
+            range(MEDIAN_COMBINATION, MAX_CONBINATION),
         )
-        thread_2.start()
-
         thread_1.join()
         thread_2.join()
+        # Get best result
         if thread_1.max_profit >= thread_2.max_profit:
-            print(f"Best: {thread_1.best_invest}")
-            best_invest = to_combination(stocks, thread_1.best_invest)
+            best_invest = to_combination(thread_1.best_invest)
             total_cost = thread_1. total_cost
-            profit = ceil(thread_1.max_profit)
+            profit = round(thread_1.max_profit, 2)
         else:
-            print(f"Best: {thread_2.best_invest}")
-            best_invest = to_combination(stocks, thread_2.best_invest)
+            best_invest = to_combination(thread_2.best_invest)
             total_cost = thread_2. total_cost
-            profit = ceil(thread_2.max_profit)
-        # Show the result of the bruteforce
+            profit = round(thread_2.max_profit, 2)
+        # Show the result
         print("Bruteforce successfully completed !\n")
         print("-------------------------\n")
         print(f"Maximum profit is {profit}€ "
@@ -123,3 +150,5 @@ if __name__ == "__main__":
         print("List of stocks to buy:")
         for stock in best_invest:
             print(f"{stock[NAME]}: {stock[PRICE]}€")
+        execution_time = round(time.time() - start_time, 3)
+        print(f"\nExecution time: {execution_time}s")
